b4a7aac7948dd6350832f5823d818f87f658d66d
Faraz <ifarazir@gmail.com>
2020-03-20 01:05:04 +0330

    Fix dual function

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
+        .dana {
+            font-family: 'dana';
+        }
+
         #imagee {
             width: 100%;
         }
@@ -18,29 +22,24 @@
 <body>
 
     <div class="container">
-        <div class="row">
-            <div class="input-group mb-3" style="margin: 15px;font-family: Dana;    display: flex;
-    justify-content: center;">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">نام تبریک گوینده</span>
+        <div class="row mt-5">
+            <div class="col-md-7 shadow rounded p-3 border">
+                <div class="form-group mb-3 text-right dana">
+                    <label for="TabrikGoo">نام تبریک گوینده</label>
+                    <input type="text" class="form-control text-right" dir="rtl" id="TabrikGoo" aria-describedby="TabrikGoo" placeholder="نام تبریک گوینده">
                 </div>
-                <input type="text" class="form-control" id="TabrikGoo" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="Mabrook">نام مبروک</span>
+                <div class="form-group mb-3 text-right dana">
+                    <label for="Mabrook">نام مبروک</label>
+                    <input type="text" class="form-control text-right" dir="rtl" id="Mabrook" aria-describedby="Mabrook" placeholder="نام مبروک">
                 </div>
-                <input type="text" class="form-control" id="Mabrook" aria-label="Sizing example input" aria-describedby="Mabrook">
-                <button class="w-100 mt-3 rounded-pill btn btn-success">ثبت</button>
-                <h1 id="download-text" style="    font-size: 30px;
-    font-weight: bold;
-    margin-top: 10px;"></h1>
+                <button class="w-100 mt-3 rounded-pill btn btn-success dana">ثبت</button>
+                <h1 id="download-text" class="dana"></h1>
             </div>
-            <div>
+            <div class="col-md-5">
                 <a download="custom-filename.jpg" href="#" id="download-link" title="ImageName">
                     <img src="Poster.jpg" id="imagee" alt="Nowruz99">
                 </a>
-
             </div>
-
         </div>
     </div>
 
@@ -50,39 +49,42 @@
     <script src="watermark.min.js" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function() {
-                    $("button").click(function() {
-                            var TabrikGoo = $('#TabrikGoo').val();
-                            var MatnTabrik = $('#MatnTabrik').val();
-                            var Mabrook = $('#Mabrook').val();
-                            $('#imagee').watermark({
-                                    text: TabrikGoo,
-                                    textWidth: 1000,
-                                    textColor: '#FFF',
-                                    textBg: 'rgba(0,0,0,0)',
-                                    opacity: 1,
-                                    gravity: 'nw',
-                                    textSize: 100,
-                                    margin: 12,
-                                    done: function(imgURL) {
-                                        this.src = imgURL;
-                                        $("#download-link").attr("href", imgURL);
-                                        $('#imagee').watermark({
-                                            text: Mabrook,
-                                            textWidth: 1000,
-                                            textColor: '#FFF',
-                                            textBg: 'rgba(0,0,0,0)',
-                                            opacity: 1,
-                                            gravity: 'se',
-                                            textSize: 100,
-                                            margin: 12,
-                                            done: function(imgURL) {
-                                                $("#download-text").append("برای دانلود روی عکس کلیک کنید");
-                                            }
-                                        });
-                                    }
-                            });
-
-                    });
+            $("button").click(function() {
+                var TabrikGoo = $('#TabrikGoo').val();
+                var MatnTabrik = $('#MatnTabrik').val();
+                var Mabrook = $('#Mabrook').val();
+                $('#imagee').watermark({
+                    text: TabrikGoo,
+                    textWidth: 1000,
+                    textColor: '#FFF',
+                    textBg: 'rgba(0,0,0,0)',
+                    opacity: 1,
+                    gravity: 'nw',
+                    textSize: 100,
+                    margin: 12,
+                    done: function(imgURL) {
+                        this.src = imgURL;
+                        $("#download-link").attr("href", imgURL);
+                        window.alert('hi1');
+                    }
+                });
+                ('#imagee').watermark({
+                    text: TabrikGoo,
+                    textWidth: 1000,
+                    textColor: '#FFF',
+                    textBg: 'rgba(0,0,0,0)',
+                    opacity: 1,
+                    gravity: 's',
+                    textSize: 100,
+                    margin: 12,
+                    done: function(imgURL) {
+                        this.src = imgURL;
+                        $("#download-link").attr("href", imgURL);
+                        window.alert('hi2');
+                    }
+                });
+            });
+        });
     </script>
 </body>
 
